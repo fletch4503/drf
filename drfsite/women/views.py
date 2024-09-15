3850dd23662712b77e7a9a6f73a7571b104b374c
fletchGitHub <fletch4503@gmail.com>
2024-09-15 14:18:16 +0300

    add some data in db + admin

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import generics
+from .models import Women
+from .serializers import WomenSerializer
 
-# Create your views here.
+
+class WomenAPIView(generics.ListAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
