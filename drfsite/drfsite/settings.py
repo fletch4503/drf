b40fd390f2a464e80cabb0af45c863fe6bf5f872
fletch4503 <fletch4503@gmail.com>
2024-10-04 14:53:39 +0300

    add module django-auth-exchange for connecting with Exchange-server via Exchangelib

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
+    "django_auth_exchange",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
@@ -102,6 +103,34 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Documentation: https://django-auth-exchange.readthedocs.io
+# Auth Exchange is a reusable Django app that allows you to authenticate users against an Exchange/Office365
+# server (using exchangelib).
+AUTHENTICATION_BACKENDS = [
+    'django_auth_exchange.backends.ExchangeAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+# Configure at least one domain
+AUTH_EXCHANGE_DOMAIN_SERVERS = {
+    'example.org': 'autodiscover',
+}
+
+# AUTH_EXCHANGE_CREATE_UNKNOWN_USER (default: True)
+#   - Determines if users should be created if they are not found in the local database.
+# AUTH_EXCHANGE_DEFAULT_DOMAIN (default: 'example.com')
+#   - If only a username is provided, this is the default domain that will be associated.
+# AUTH_EXCHANGE_ALLOWED_FORMATS (default: ['email', 'netbios', 'username'])
+#   - This specifies which formats are allowed as the username (email means someuser@example.com,
+#       netbios means EXAMPLE\someuser, and username means someuser).
+# AUTH_EXCHANGE_DOMAIN_SERVERS (default: {})
+#   - This specifies the domains which are allowed to authenticate and
+#       the server that should be used for authentication (or 'autodiscover').
+#       Hint: Office365 uses the server outlook.office365.com.
+# AUTH_EXCHANGE_DOMAIN_USER_PROPERTIES (default: {})
+#   - This specifies the settings we should apply to a user when they are added to the local database for each domain
+#   (e.g., to make all example.com users superusers, do: {'example.com': {'is_staff': True, 'is_superuser': True}}).
+# AUTH_EXCHANGE_NETBIOS_TO_DOMAIN_MAP (default: {}) - This specifies a mapping from NETBIOS names to domain names.
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
