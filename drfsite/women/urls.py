0a75325ea454ffba1e4c827db07eee72c58fd856
fletchGitHub <fletch4503@gmail.com>
2024-10-05 14:19:10 +0300

    Move drf on DellAIO

--- drfsite/women/urls.py
+++ drfsite/women/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path("list/", views.WomenListView.as_view(), name="list"),  # Список всех женщин - отображение на основе класса
     path("<int:pk>/", views.WomenDetailView.as_view(), name="detail"),  # Детальный вид по элементу из списка
     re_path(r'about', views.about, name="about"),
-    path("actress/", include(actress_patterns)),
     path("add/", views.CreateWomenView.as_view(), name="add"),
     # path("add/", views.postwoman, name="add"),
 
