4ca35d84faabc4366c39db86dcc45354c1e31a54
fletch4503 <fletch4503@gmail.com>
2024-10-15 13:45:57 +0300

    add admin view all ewsitem, including archived

--- drfsite/ews_list/admin.py
+++ drfsite/ews_list/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import *
+from ews_list.models import ewsitem
 
-admin.site.register(ewsitem)
+
+# admin.site.register(ewsitem)
 # Register your models here.
+
+@admin.register(ewsitem)
+class ewsitemAdmin(admin.ModelAdmin):
+    list_display = "id", "email_title", "sender", "visible", "done", "time_receive", "cat"
+    list_display_links = "id", "email_title", "time_receive"
+
+    def visible(self, obj: ewsitem) -> bool:
+        return not obj.archived
+
+    visible.boolean = True
